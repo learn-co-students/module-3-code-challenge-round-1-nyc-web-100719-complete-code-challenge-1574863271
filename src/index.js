904b29f357053299c8c473e450b721de39871f39
hvtsxyz <hvts@msn.com>
2019-11-27 10:39:39 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4039 //Enter the id from the fetched image here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
   const likeURL = `https://randopic.herokuapp.com/likes/`
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
+
+  function fetchImage () {
+    fetch(imageURL).then(function(response) {
+     return response.json();
+   })
+   .then(function(json) {
+     console.log(json)
+    let imgName = document.getElementById('name');
+    imgName.innerText = `${json.name}`
+    let imgSrc = document.getElementById('image').src = `${json.url}`
+    let imgLikes = document.getElementById('likes');
+    imgLikes.innerText = `${json.like_count}`
+    
+
+   });
+  }
+  fetchImage();
+
+  function persistLike(id, like, e){
+    fetch(`https://randopic.herokuapp.com/images/${imageId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accepts: "application/json"
+      },
+      body: JSON.stringify({likes: like})
+    })
+  }
+
+  document.addEventListener("click", function(e){
+    if (e.target.dataset.purpose === "like"){
+      let parent = e.target.parentNode
+      let p = document.getElementById("likes")
+      let newLike = parseInt(p.innerText) + 1
+      e.target.dataset.likes = newLike
+      p.innerText = newLike
+      let id = e.target.dataset.id
+      persistLike(id, newLike, e)
+    }
+  })
+
+ 
+  
 
 })
